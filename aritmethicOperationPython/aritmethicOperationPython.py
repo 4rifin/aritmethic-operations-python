41b191c6004da4b56f5869079ff6941759da9d18
Arifin <35588262+4rifin@users.noreply.github.com>
2024-06-05 14:06:55 +0800

    commite
    commite aritmethic Operation Python

---
+++ aritmethicOperationPython/aritmethicOperationPython.py
@@ -0,0 +1,29 @@
+#Arithmetic Operators
+print('==============Arithmetic Operators =============')
+firstNumber = 8
+secondNumber = 3
+#Addition
+resultAddition =  firstNumber + secondNumber
+#Subtraction
+resultSubtraction = firstNumber - secondNumber
+#Multiplication
+resultMultiplication = firstNumber * secondNumber
+#Division
+resultDivision = firstNumber / secondNumber
+#Floor Division
+resultFloorDivision = firstNumber // secondNumber
+#Modulus
+resultModulus = firstNumber % secondNumber
+#Exponent
+resultExponent = firstNumber ** secondNumber
+
+print('First Number:',firstNumber)
+print('Second Number:',secondNumber)
+print('==============Result =============')
+print('Addition (+):',resultAddition)
+print('Subtraction (-):',resultSubtraction)
+print('Multiplication (*):',resultMultiplication)
+print('Division (/):',resultDivision)
+print('Floor Division (//):',resultFloorDivision)
+print('Modulus (%):',resultModulus)
+print('Exponent (**):',resultExponent)
